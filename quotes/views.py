2606e246c1975e22891b5983b5bfbb6dac94ed62
Ivan Yang <yangi@bu.edu>
2024-09-24 17:42:42 -0400

    Added pics/quotes, and first view

--- quotes/views.py
+++ quotes/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+import random
 # Create your views here.
+quotes = [
+    "I've always believed that success for anyone is all about drive, dedication, and desire, but for me, it's also been about confidence and faith.",
+    "We're going to have to work. We're going to have to play well. Nothing will be given to us.",
+    "I always have an optimistic view, no matter what it is."
+]
+
+images = [
+    'pics/pic1.jpg',
+    'pics/pic2.webp',
+    'pics/pic3.webp'
+]
+
+def quote(request):
+    random_quote = random.choice(quotes)
+    random_image = random.choice(images)
+    return render(request, 'quote.html', {'quote': random_quote, 'image': random_image})
