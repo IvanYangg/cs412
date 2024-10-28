3b66b200ad94c8ff52d4ddb618a642e0f94d1582
Ivan Yang <yangi@bu.edu>
2024-10-28 15:51:16 -0400

    Implemented Friends functionality

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -4,10 +4,12 @@
 
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 # a custom profile model that defines the first_name, last_name, city, email, and profile_image_url data attributes of a generic user. 
 class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     first_name = models.CharField(max_length=100, blank=False)
     last_name = models.CharField(max_length=100, blank=False)
     city = models.CharField(max_length=100, blank=False)
@@ -22,6 +24,11 @@ class Profile(models.Model):
     
     def get_absolute_url(self):
         return reverse('show_profile', kwargs={'pk': self.pk})
+    
+    def get_friends(self):
+        friends1 = Friend.objects.filter(profile1=self).values_list('profile2', flat=True)
+        friends2 = Friend.objects.filter(profile2=self).values_list('profile1', flat=True)
+        return Profile.objects.filter(id__in=list(friends1) + list(friends2))
 
 # a custom profile model that defines status messages that are linked to a profile. Has data attributes, timestamp, message, and profile, which is the foreign key. 
 class StatusMessage(models.Model):
@@ -40,3 +47,12 @@ class Image(models.Model):
     image_file = models.ImageField(blank=True) 
     status_message = models.ForeignKey('StatusMessage', on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now=True)
+
+# a custom model for friends functionality
+class Friend(models.Model):
+    profile1 = models.ForeignKey(Profile, related_name="profile1", on_delete=models.CASCADE)
+    profile2 = models.ForeignKey(Profile, related_name="profile2", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.profile1.user.username} & {self.profile2.user.username}"
