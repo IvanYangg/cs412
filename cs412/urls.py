f7121cd4b1de6248527ec9337a8e05db6dba252e
Ivan Yang <yangi@bu.edu>
2024-11-19 14:07:02 -0500

    setup for final project

--- cs412/urls.py
+++ cs412/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('restaurant/', include('restaurant.urls')),
     path('mini_fb/', include('mini_fb.urls')),
     path('voter_analytics/', include('voter_analytics.urls')),
+    path('project/', include('project.urls')),
 ]
 # include media directory
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
